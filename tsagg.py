eff94f0e38c67efae1dd8ceb32429452d6fd5976
kbrannan <brannan.kevin@deq.state.or.us>
2019-07-19 12:30:58 -0700

    created function to aggregate the time-series using min, mean, and max functions for the hour and al the reaches

---
+++ tsagg.py
@@ -0,0 +1,9 @@
+def tsagg(df, wq_par):
+    df['Hour'] = df['Time'].astype(int)
+    df['Hour'] = df['Hour'].astype(str)
+    df['Hour'] = df['Hour'].astype(int)
+    agg_funcs = dict(Min='min', Ave='mean', Max='max')
+    df_agg = df.groupby(['Reach', 'Hour'])[wq_par].agg(agg_funcs)
+    df_agg.reset_index(inplace=True)
+    return(df_agg)
+
