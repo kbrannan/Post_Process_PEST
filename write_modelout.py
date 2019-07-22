71d8cb9ea624fc7fa1f7ef6547665b50c4e2d58c
kbrannan <brannan.kevin@deq.state.or.us>
2019-07-22 15:51:22 -0700

    checked if function works when returning a dat frame

--- write_modelout.py
+++ write_modelout.py
@@ -1,6 +1,7 @@
 import re
+import pandas as pd
 
-# reads the model.ins file to get the obs names and formart in a disctionary
+# reads the model.ins file to get the obs names, formart, and -1 for vals in a data frame
 def get_modelin(fn):
     d = {} # dictionary containing the obs name and the boundaries in the line
     with open(fn) as f:
@@ -8,5 +9,14 @@ def get_modelin(fn):
         for line in f:
             (key, val) = re.sub('(^.*\[)|( .*$\n)','', line).split(']') # mung the line and retirn ons name and boundaries
             d[key] = val
-    return(d)
+    df = pd.DataFrame(d.items(), columns=('Name','cols')) # make data frame from dictionary
+    df['vals'] = -1.0 # add col for vals with defaults of -1.0
+    return(df)
 
+junk = pd.DataFrame.from_dict(d, orient='index')
+list(junk)
+junk = pd.DataFrame(d.items(), columns=('Name','cols'))
+junk['vals'] = -1
+junk.head()
+junk = get_modelin('./qual2kw_files/model.ins')
+junk.tail()
