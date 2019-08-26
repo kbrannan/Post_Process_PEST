0a66da5302ce3657c1035b5595cd4d672a151fc8
kbrannan <brannan.kevin@deq.state.or.us>
2019-08-26 09:20:39 -0700

    make clean repo

--- scripts/model_ins_chk.bat
+++
@@ -1,4 +0,0 @@
-REM run inschek to see if model.out from Postprocess_For_Pest in correct format for PEST
-cd C:\Users\kbranna\local_repos\qual2kw_output\qual2kw_files
-inschek.exe model.ins model.out   
-REM c:\PEST\inschek.exe C:\Users\kbranna\local_repos\qual2kw_output\qual2kw_files\model.ins C:\Users\kbranna\local_repos\qual2kw_output\qual2kw_files\model.out   
