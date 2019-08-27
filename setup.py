c3ae5355d8f1cee38cda657124e2619850c39759
Kevin Brannan <kbrannan@users.noreply.github.com>
2019-08-27 10:41:47 -0700

    trying to get version for correct

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     # Needed for dependencies
-    install_requires=['re==2.1.1', 'pandas==0.24.2'],
+    install_requires = ['re>2.1', 'pandas==0.24'],
+    install_requires = ['django-pipeline==1.1.22', 'south>=0.7']
     # The license can be anything you like
     license='MIT',
     description='An example of a python package from pre-existing code',
